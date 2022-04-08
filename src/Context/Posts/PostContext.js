c05fa11c82462a6b06c4c2d3a63b2120a401a9f7
Keval <kevalpm@gmail.com>
2022-04-09 00:21:00 +0530

    loggen in user's name displayed

--- src/Context/Posts/PostContext.js
+++ src/Context/Posts/PostContext.js
@@ -8,6 +8,7 @@ export const PostProvider = (props) => {
     const [posts, setPosts] = useState([])
     const [onePost, setOnePost] = useState([])
     const [amPost, setAmPost] = useState([])
+    const [userInfo, setUserInfo] = useState({})
     const [fullPostID, setFullPostID] = useState(null)
 
     // Add Post
@@ -96,6 +97,18 @@ export const PostProvider = (props) => {
         return response
     }
 
+    // Fetch user info http://localhost:5000/api/auth/getuser
+    const fetchUserInfo = async () => {
+        const response = await fetch('http://localhost:5000/api/auth/getuser', {
+            method: 'POST',
+            headers: {
+                'auth-token': localStorage.getItem(AUTH_STORAGE_KEY)
+            }
+        })
+        const res = await response.json()
+        setUserInfo(res)
+    }
+
     // Logout
     const LogOutFunc = () => {
         localStorage.removeItem(AUTH_STORAGE_KEY)
@@ -103,7 +116,7 @@ export const PostProvider = (props) => {
     }
 
     return (
-        <PostContext.Provider value={{ amPost, fetchAMPost, onePost, fetchOnePost, AddPostFunc, signUpFunc, logInFunc, fetchAllPosts, posts, fullPostID, setFullPostID, LogOutFunc }} >
+        <PostContext.Provider value={{ userInfo, fetchUserInfo, amPost, fetchAMPost, onePost, fetchOnePost, AddPostFunc, signUpFunc, logInFunc, fetchAllPosts, posts, fullPostID, setFullPostID, LogOutFunc }} >
             {props.children}
         </PostContext.Provider>
     )
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,18 +1,30 @@
-import React from 'react'
-import { useLocation } from 'react-router-dom'
+import React, { useContext, useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
+import PostContext from '../Context/Posts/PostContext'
 import profileImage from '../images/test_profile.jpg'
 
 const Profile = () => {
-    const location = useLocation()
+    const context = useContext(PostContext)
+    const { userInfo, fetchUserInfo } = context
+    const nevigate = useNavigate()
+
+    useEffect(() => {
+        if (localStorage.getItem('Waste_Aid_authtoken')) {
+            fetchUserInfo()
+        } else {
+            nevigate('/login')
+        }
+        // eslint-disable-next-line
+    }, [])
     return (
-        (location.pathname !== '/login' && location.pathname !== '/signup') &&
+        (localStorage.getItem('Waste_Aid_authtoken')) &&
         <>
             <section className="profile__main">
                 <div className="profile__container">
                     <div className="profile__image">
                         <img src={profileImage} alt="WasteAid User profile" />
                     </div>
-                    <p className="profile__name">John Doe</p>
+                    <p className="profile__name">{userInfo && userInfo.fname + ' ' + userInfo.lname}</p>
                 </div>
             </section>
         </>
