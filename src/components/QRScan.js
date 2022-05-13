d5e74263cc271e4efd76fdb22c88bbb73c3ff6be
Keval <kevalpm@gmail.com>
2022-05-13 11:39:30 +0530

    qr-scanner facingMode added

--- src/components/QRScan.js
+++ src/components/QRScan.js
@@ -1,36 +1,37 @@
-import React from 'react'
-import QrReader from 'react-qr-scanner'
+import React from 'react';
+import QrReader from 'react-qr-scanner';
 import { useNavigate } from 'react-router-dom';
 
 const QRScan = () => {
-    const nevigate = useNavigate()
+  const nevigate = useNavigate();
 
-    const handleScanWebcam = async (result) => {
-        if (result) {
-            nevigate(`/qrprocessing/${result.text}`)
-        }
+  const handleScanWebcam = async (result) => {
+    if (result) {
+      nevigate(`/qrprocessing/${result.text}`);
     }
-    const handleErrorWebcam = (error) => {
-        console.log(error)
-    }
-    const style = {
-        'borderRadius': '10px',
-        'width': '90%',
-        'margin': '1rem'
-    }
-    return (
-        <>
-            <section className="center__component">
-                <h2 className='mx-2'>Scan the QR code</h2>
-                <QrReader
-                    style={style}
-                    delay={300}
-                    onError={handleErrorWebcam}
-                    onScan={handleScanWebcam}
-                />
-            </section>
-        </>
-    )
-}
+  };
+  const handleErrorWebcam = (error) => {
+    console.log(error);
+  };
+  const style = {
+    borderRadius: '10px',
+    width: '90%',
+    margin: '1rem'
+  };
+  return (
+    <>
+      <section className="center__component">
+        <h2 className="mx-2">Scan the QR code</h2>
+        <QrReader
+          style={style}
+          delay={300}
+          onError={handleErrorWebcam}
+          onScan={handleScanWebcam}
+          facingMode={'rear'}
+        />
+      </section>
+    </>
+  );
+};
 
-export default QRScan
+export default QRScan;
